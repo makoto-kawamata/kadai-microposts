6f85063fd538faf2a8f33e2fc5939866c15d254f
makoto.kawamata <kawaman.program@gmail.com>
2019-01-23 20:54:47 +0900

    follow, unfollow

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,16 @@
+class RelationshipsController < ApplicationController
+  before_action :require_user_logged_in
+  def create
+    user = User.find(params[:follow_id])
+    current_user.follow(user)
+    flash[:success] = 'ユーザをフォローしました。'
+    redirect_to user
+  end
+
+  def destroy
+    user = User.find(params[:follow_id])
+    current_user.unfollow(user)
+    flash[:success] = 'ユーザのフォローを解除しました。'
+    redirect_to user
+  end
+end
